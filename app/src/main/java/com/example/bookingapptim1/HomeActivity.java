ad774ffc715042f61570b3dbd1e75528a2fcfb04
Momir Milutinović <milutinm462@gmail.com>
2023-11-10 20:42:17 +0100

    feat: Login screen, splash screen and project initialization

---
+++ app/src/main/java/com/example/bookingapptim1/HomeActivity.java
@@ -0,0 +1,61 @@
+package com.example.bookingapptim1;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.bookingapptim1.databinding.ActivityHomeBinding;
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HomeActivity extends AppCompatActivity {
+    private ActivityHomeBinding binding;
+    private AppBarConfiguration mAppBarConfiguration;
+    private DrawerLayout drawer;
+    private NavigationView navigationView;
+    private NavController navController;
+    private Toolbar toolbar;
+    private ActionBar actionBar;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private Set<Integer> topLevelDestinations = new HashSet<>();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        drawer = binding.drawerLayout;
+        navigationView = binding.navView;
+        toolbar = binding.activityHomeBase.toolbar;
+
+        setSupportActionBar(toolbar);
+        actionBar = getActionBar();
+        if(actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(false);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_hamburger);
+            actionBar.setHomeButtonEnabled(true);
+        }
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+    }
+    @Override
+    public boolean onSupportNavigateUp() {
+        navController = Navigation.findNavController(this, R.id.fragment_nav_content_main);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration) || super.onSupportNavigateUp();
+    }
+}
---
+++ app/src/main/java/com/example/bookingapptim1/SplashScreenActivity.java
@@ -0,0 +1,28 @@
+package com.example.bookingapptim1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreenActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        int SPLASH_SCREEN_TIMEOUT = 5000;
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, LoginScreenActivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+        }, SPLASH_SCREEN_TIMEOUT);
+    }
+}
+

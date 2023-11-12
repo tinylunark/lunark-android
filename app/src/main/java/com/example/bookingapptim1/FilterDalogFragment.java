b183484a99b45611c037c762618bfe7ed92fccbd
Dusan Lecic <42711489+dusanlecic@users.noreply.github.com>
2023-11-12 20:02:41 +0100

    add: filter dialog modal

--- app/src/main/java/com/example/bookingapptim1/FilterDalogFragment.java
+++ app/src/main/java/com/example/bookingapptim1/FilterDalogFragment.java
@@ -1,22 +1,18 @@
 package com.example.bookingapptim1;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FilterDalogFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class FilterDalogFragment extends Fragment {
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+public class FilterDalogFragment extends DialogFragment {
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -28,15 +24,27 @@ public class FilterDalogFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FilterDalogFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstance){
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+        builder.setTitle("Filter")
+                .setMessage("Add your filter options here")
+                .setPositiveButton("Apply", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        getExitTransition();
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        getExitTransition();
+                    }
+                });
+
+                return builder.create();
+    }
     public static FilterDalogFragment newInstance(String param1, String param2) {
         FilterDalogFragment fragment = new FilterDalogFragment();
         Bundle args = new Bundle();

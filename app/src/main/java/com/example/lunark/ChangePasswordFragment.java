347b509c5050849e1a9295c843c95fe478ae7815
Dusan Lecic <42711489+dusanlecic@users.noreply.github.com>
2023-11-16 18:59:15 +0100

    add: change password fragment

---
+++ app/src/main/java/com/example/lunark/ChangePasswordFragment.java
@@ -0,0 +1,26 @@
+package com.example.lunark;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class ChangePasswordFragment extends Fragment {
+
+    public ChangePasswordFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_change_password, container, false);
+
+        // TODO: Initialize views and handle button clicks
+
+        return view;
+    }
+}

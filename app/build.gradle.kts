fc619a322bf30c353f2a6e9e1ed5127fc7a9239c
Vladislav Radovic <vradovic522@gmail.com>
2023-12-14 15:23:10 +0100

    task: setup retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,11 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+
 plugins {
     id("com.android.application")
+}
+
+fun getIpAddress(): String {
+    return gradleLocalProperties(rootDir).getProperty("ip_addr");
 }
 
 android {
@@ -23,9 +29,12 @@ android {
         release {
             isMinifyEnabled = false
             proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
             )
+        }
+        debug {
+            buildConfigField("String", "IP_ADDR", "\"" + getIpAddress() + "\"")
         }
     }
     compileOptions {
@@ -36,6 +45,7 @@ android {
     buildFeatures {
         viewBinding = true
         compose = true
+        buildConfig = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.3"
@@ -72,4 +82,8 @@ dependencies {
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation("com.google.code.gson:gson:2.8.7")
+    implementation("com.squareup.retrofit2:retrofit:2.3.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.3.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:3.12.1")
 }

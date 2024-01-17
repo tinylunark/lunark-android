81ad8ba02e9341fac9960d57afe261e574251aa4
Momir Milutinović <milutinm462@gmail.com>
2024-01-17 13:50:57 +0100

    fix: Make data binding work in WriteReviewFragment

--- app/src/main/java/com/example/lunark/fragments/WriteReviewFragment.java
+++ app/src/main/java/com/example/lunark/fragments/WriteReviewFragment.java
@@ -9,18 +9,24 @@ import android.widget.RatingBar;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.BindingAdapter;
+import androidx.databinding.DataBindingUtil;
 import androidx.databinding.InverseBindingAdapter;
 import androidx.databinding.InverseBindingMethod;
 import androidx.databinding.InverseBindingMethods;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.lunark.R;
 import com.example.lunark.databinding.FragmentWriteReviewBinding;
 import com.example.lunark.models.ReviewType;
 import com.example.lunark.viewmodels.PropertyDetailViewModel;
 import com.example.lunark.viewmodels.ReviewViewModel;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.Objects;
+
+import io.reactivex.CompletableObserver;
+import io.reactivex.disposables.Disposable;
 
 public class WriteReviewFragment extends Fragment {
 
@@ -47,7 +53,8 @@ public class WriteReviewFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        binding = FragmentWriteReviewBinding.inflate(inflater, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_write_review, container, false);
+        binding.setViewmodel(viewModel);
         return binding.getRoot();
     }
 }

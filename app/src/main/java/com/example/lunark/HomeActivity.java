d11b6bd7a93632316ac7670743ec71f38966e1e2
Dusan Lecic <42711489+dusanlecic@users.noreply.github.com>
2024-01-17 18:53:31 +0100

    feat: home activity cleanup

--- app/src/main/java/com/example/lunark/HomeActivity.java
+++ app/src/main/java/com/example/lunark/HomeActivity.java
@@ -7,8 +7,6 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
@@ -16,29 +14,20 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.MenuProvider;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentResultListener;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.lifecycle.Lifecycle;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import com.example.lunark.databinding.ActivityHomeBinding;
-import com.example.lunark.fragments.FiltersDialogFragment;
-import com.example.lunark.fragments.createProperty.CreatePropertyFragment;
 import com.example.lunark.fragments.createProperty.IAllowBackPressed;
 import com.example.lunark.models.Login;
-import com.example.lunark.models.Property;
 import com.example.lunark.notifications.NotificationReceiver;
-import com.example.lunark.repositories.LoginRepository;
-import com.example.lunark.fragments.PropertiesFragment;
-import com.example.lunark.fragments.PropertyDetailFragment;
-import com.example.lunark.models.Login;
 import com.example.lunark.repositories.LoginRepository;
 import com.google.android.material.navigation.NavigationView;
 
@@ -120,23 +109,6 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public void onSubscribe(Disposable d) {
-                subscription = d;
-            }
-
-            @Override
-            public void onSuccess(Login login) {
-                String userRole = loginRepository.extractRoleFromJwt(login.getAccessToken());
-                setupNavigationMenu(userRole);
-            }
-
-            @Override
-            public void onError(Throwable e) {
-                e.printStackTrace();
-            }
-        });
     }
     @Override
     protected void onDestroy() {
@@ -183,54 +155,6 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             public void onError(Throwable e) {
                 e.printStackTrace();
-            }
-        });
-    }
-
-    private void setupNavigationMenu(String userRole) {
-        Log.d("NAV", "Setting up navigation menu for role: " + userRole);
-        navigationView.getMenu().clear();
-        int menuResId = R.menu.nav_menu;
-        if (userRole != null) {
-            switch (userRole) {
-                case "ADMIN":
-                    menuResId = R.menu.admin_nav_menu;
-                    break;
-                case "HOST":
-                    menuResId = R.menu.host_nav_menu;
-                    break;
-                case "GUEST":
-                    menuResId = R.menu.nav_menu;
-                    break;
-            }
-        }
-
-        navigationView.inflateMenu(menuResId);
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int itemId = item.getItemId();
-
-                if (itemId == R.id.menu_home) {
-                    if (!isActivityRunning(HomeActivity.class)) {
-                        Intent homeIntent = new Intent(HomeActivity.this, HomeActivity.class);
-                        startActivity(homeIntent);
-                    }
-                } else if (itemId == R.id.menu_account) {
-                    if (!isActivityRunning(AccountScreen.class)) {
-                        Intent accountIntent = new Intent(HomeActivity.this, AccountScreen.class);
-                        startActivity(accountIntent);
-                    }
-                } else if (itemId == R.id.menu_reservations_host) {
-                    if (!isActivityRunning(HostReservationScreenActivity.class)) {
-                        Intent accountIntent = new Intent(HomeActivity.this, HostReservationScreenActivity.class);
-                        startActivity(accountIntent);
-                    }
-                } else if (itemId == R.id.menu_logout) {
-                    logOut();
-                }
-                drawer.closeDrawers();
-                return true;
             }
         });
     }

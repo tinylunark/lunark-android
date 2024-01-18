3cf79086157c2ac1b929d5a4ed17967f282da046
Dusan Lecic <42711489+dusanlecic@users.noreply.github.com>
2024-01-18 20:29:50 +0100

    hotfix: duplicate inject

--- app/src/main/java/com/example/lunark/ApplicationComponent.java
+++ app/src/main/java/com/example/lunark/ApplicationComponent.java
@@ -3,31 +3,31 @@ package com.example.lunark;
 
 import android.content.Context;
 
+import com.example.lunark.adapters.ReservationsCancelListAdapter;
 import com.example.lunark.adapters.ReservationsListAdapter;
 import com.example.lunark.adapters.ReservationsListAdapterBase;
-import com.example.lunark.adapters.ReservationsCancelListAdapter;
 import com.example.lunark.adapters.ReviewListAdapter;
 import com.example.lunark.adapters.UnapprovedPropertiesListAdapter;
 import com.example.lunark.datasources.DiskModule;
 import com.example.lunark.datasources.NetworkModule;
+import com.example.lunark.fragments.AllReservationsFragment;
 import com.example.lunark.fragments.FavoritePropertiesFragment;
 import com.example.lunark.fragments.GeneralReportFragment;
+import com.example.lunark.fragments.GuestCancelReservationFragment;
 import com.example.lunark.fragments.GuestSettingsFragment;
 import com.example.lunark.fragments.HostSettingsFragment;
 import com.example.lunark.fragments.NotificationsFragment;
 import com.example.lunark.fragments.PendingReservationsFragment;
-import com.example.lunark.fragments.AllReservationsFragment;
-import com.example.lunark.fragments.GuestCancelReservationFragment;
-import com.example.lunark.repositories.ReservationRepository;
-import com.example.lunark.viewmodels.*;
-import com.example.lunark.fragments.PropertyDetailFragment;
 import com.example.lunark.fragments.PropertyApprovalFragment;
+import com.example.lunark.fragments.PropertyDetailFragment;
 import com.example.lunark.fragments.PropertyReportFragment;
 import com.example.lunark.notifications.NotificationService;
 import com.example.lunark.repositories.PropertyRepository;
+import com.example.lunark.repositories.ReservationRepository;
 import com.example.lunark.viewmodels.HostViewModel;
-import com.example.lunark.viewmodels.PropertiesViewModel;
+import com.example.lunark.viewmodels.ReservationsViewModel;
 import com.example.lunark.viewmodels.ReviewViewModel;
+import com.example.lunark.viewmodels.UnapprovedPropertiesViewModel;
 
 import javax.inject.Singleton;
 
@@ -64,8 +64,6 @@ public interface ApplicationComponent {
     void inject(ReviewListAdapter reviewListAdapter);
     void inject(GuestSettingsFragment guestSettingsFragment);
     void inject(HostSettingsFragment hostSettingsFragment);
-    void inject(PropertyApprovalFragment propertyApprovalFragment);
-    void inject(UnapprovedPropertiesListAdapter unapprovedPropertiesListAdapter);
     void inject(ReservationRepository reservationRepository);
     void inject(PropertyApprovalFragment propertyApprovalFragment);
     void inject(UnapprovedPropertiesListAdapter unapprovedPropertiesListAdapter);

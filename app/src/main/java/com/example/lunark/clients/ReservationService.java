3f11503442a3a521c7d1db740120c19c48022dd8
Dusan Lecic <42711489+dusanlecic@users.noreply.github.com>
2024-01-18 20:33:08 +0100

    fix: forgotten import

--- app/src/main/java/com/example/lunark/clients/ReservationService.java
+++ app/src/main/java/com/example/lunark/clients/ReservationService.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;

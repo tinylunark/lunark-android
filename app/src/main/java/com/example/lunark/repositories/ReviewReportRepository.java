43db1c99d7b49ac886ad317985ec56798d8f7a9c
Dusan Lecic <42711489+dusanlecic@users.noreply.github.com>
2024-01-19 13:22:59 +0100

    add: review report repository

---
+++ app/src/main/java/com/example/lunark/repositories/ReviewReportRepository.java
@@ -0,0 +1,76 @@
+package com.example.lunark.repositories;
+
+import android.util.Log;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.lunark.models.ReviewReport;
+import com.example.lunark.services.ReviewReportService;
+import com.example.lunark.util.ClientUtils;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
+public class ReviewReportRepository {
+    private static final String LOG_TAG = "ReviewReportRepository";
+    ReviewReportService reviewReportService;
+
+    @Inject
+    public ReviewReportRepository() {
+        this.reviewReportService = ClientUtils.reviewReportService;
+    }
+
+    @Inject
+    public void setRetrofit(Retrofit retrofit) {
+        this.reviewReportService = retrofit.create(ReviewReportService.class);
+    }
+
+    public LiveData<List<ReviewReport>> getReviewReports() {
+        final MutableLiveData<List<ReviewReport>> data = new MutableLiveData<>();
+
+        reviewReportService.getReviewReports().enqueue(new Callback<List<ReviewReport>>() {
+            @Override
+            public void onResponse(Call<List<ReviewReport>> call, Response<List<ReviewReport>> response) {
+                if (response.isSuccessful()) {
+                    List<ReviewReport> ReviewReports = response.body();
+                    data.setValue(response.body());
+                } else {
+                    Log.w(LOG_TAG, "Get ReviewReports response not successful: " + response.code());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<ReviewReport>> call, Throwable t) {
+                Log.e(LOG_TAG, "Get ReviewReports failure: " + t.getMessage());
+            }
+        });
+
+        return data;
+    }
+
+
+    public void deleteReview(Long ReviewReportId) {
+        reviewReportService.deleteReview(ReviewReportId).enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if (response.isSuccessful()) {
+                    Log.e(LOG_TAG, "Accept ReviewReport success: " + response.code());
+                } else {
+                    Log.e(LOG_TAG, "Accept ReviewReport error: " + response.code());
+                }
+            }
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Log.e(LOG_TAG, "Accept ReviewReport failure: " + t.getMessage());
+            }
+        });
+    }
+}

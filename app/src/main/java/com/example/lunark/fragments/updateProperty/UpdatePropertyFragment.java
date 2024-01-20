72f4f8272ecbcaca25ecb89a7a970b8449ef8a8f
Dusan Lecic <42711489+dusanlecic@users.noreply.github.com>
2024-01-20 17:35:53 +0100

    add: update property package with fragments

---
+++ app/src/main/java/com/example/lunark/fragments/updateProperty/UpdatePropertyFragment.java
@@ -0,0 +1,19 @@
+package com.example.lunark.fragments.updateProperty;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.lunark.R;
+
+public class UpdatePropertyFragment extends Fragment {
+
+
+    public UpdatePropertyFragment() {
+    }
+
+}
